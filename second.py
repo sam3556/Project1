b14eb8f4bac0e16f9a29a46938926085455fd012
sam3556 <73242558+sam3556@users.noreply.github.com>
2020-10-26 21:12:35 +0530

    Add files via upload
    pycharm upload 2

---
+++ second.py
@@ -0,0 +1,6 @@
+
+word= "shanky"
+gate_score = "700"
+rank = "500"
+print("gate exam was conducted in the year 2020\nit was the first attempt of "+word)
+print("he scored "+ gate_score + " and his rank was "+rank)
